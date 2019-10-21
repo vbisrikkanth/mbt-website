1b6aa773ffd37763cb6470168dc4ffb48479635e
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-10-21 08:40:27 +0700

    Replace slider

--- src/pages/screenshots/index.js
+++ src/pages/screenshots/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Carousel from 'nuka-carousel';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 import Layout from '@theme/Layout';
 import withBaseUrl from '@docusaurus/withBaseUrl';
+import styles from './styles.module.css';
+import classnames from 'classnames';
 
 function Screenshots() {
-  setTimeout(() => {
-    window.dispatchEvent(new Event('resize'));
-  }, 200);
   return (
     <Layout title="Screenshots">
-      <div className="container">
-        <Carousel>
-          <img src={withBaseUrl('img/screenshots/1.dashboard.png')} />
-          <img src={withBaseUrl('img/screenshots/2.tasks.png')} />
-          <img src={withBaseUrl('img/screenshots/3.bugs.png')} />
-          <img src={withBaseUrl('img/screenshots/4.models.png')} />
-          <img src={withBaseUrl('img/screenshots/5.predefined-cases.png')} />
-          <img src={withBaseUrl('img/screenshots/6.users.png')} />
-        </Carousel>
+      <div className={classnames('container', styles.bulletsSpace)}>
+        <AwesomeSlider>
+          <div data-src={withBaseUrl('img/screenshots/1.dashboard.png')} />
+          <div data-src={withBaseUrl('img/screenshots/2.tasks.png')} />
+          <div data-src={withBaseUrl('img/screenshots/3.bugs.png')} />
+          <div data-src={withBaseUrl('img/screenshots/4.models.png')} />
+          <div data-src={withBaseUrl('img/screenshots/5.predefined-cases.png')} />
+          <div data-src={withBaseUrl('img/screenshots/6.users.png')} />
+        </AwesomeSlider>
       </div>
     </Layout>
   );
