2dd17cc2ea95fea6019f5a31e7dba0c1d625f8ff
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-10-22 10:24:20 +0700

    Update version of docusaurus 2

--- src/pages/screenshots/index.js
+++ src/pages/screenshots/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import Layout from '@theme/Layout';
-import withBaseUrl from '@docusaurus/withBaseUrl';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import classnames from 'classnames';
 
@@ -11,12 +11,12 @@ function Screenshots() {
     <Layout title="Screenshots">
       <div className={classnames('container', styles.bulletsSpace)}>
         <AwesomeSlider>
-          <div data-src={withBaseUrl('img/screenshots/1.dashboard.png')} />
-          <div data-src={withBaseUrl('img/screenshots/2.tasks.png')} />
-          <div data-src={withBaseUrl('img/screenshots/3.bugs.png')} />
-          <div data-src={withBaseUrl('img/screenshots/4.models.png')} />
-          <div data-src={withBaseUrl('img/screenshots/5.predefined-cases.png')} />
-          <div data-src={withBaseUrl('img/screenshots/6.users.png')} />
+          <div data-src={useBaseUrl('img/screenshots/1.dashboard.png')} />
+          <div data-src={useBaseUrl('img/screenshots/2.tasks.png')} />
+          <div data-src={useBaseUrl('img/screenshots/3.bugs.png')} />
+          <div data-src={useBaseUrl('img/screenshots/4.models.png')} />
+          <div data-src={useBaseUrl('img/screenshots/5.predefined-cases.png')} />
+          <div data-src={useBaseUrl('img/screenshots/6.users.png')} />
         </AwesomeSlider>
       </div>
     </Layout>
